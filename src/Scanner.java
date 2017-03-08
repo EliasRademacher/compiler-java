cdda715d185168f8f135488691e6ab6e184dcc2b
EliasRademacher <rademace@onid.oregonstate.edu>
2017-03-08 15:41:58 -0800

    Added getToken() method to Scanner.

--- src/Scanner.java
+++ src/Scanner.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
+import java.lang.Character;
 
 
 /**
@@ -11,7 +12,7 @@ import java.util.HashMap;
 public class Scanner {
 
     /* States in scanner DFA */
-    public enum StateType {
+    public enum State {
         START, INASSIGN, INCOMMENT, INNUM, INID, DONE
     }
 
@@ -37,18 +38,18 @@ public class Scanner {
     private boolean EOF_flag = false;
 
     /* lookup table of reserved words */
-    private HashMap<String, TokenType> reservedWords;
+    private HashMap<String, Token> reservedWords;
 
     public Scanner() {
         reservedWords = new HashMap<>();
-        reservedWords.put("if", TokenType.IF);
-        reservedWords.put("then", TokenType.THEN);
-        reservedWords.put("else", TokenType.ELSE);
-        reservedWords.put("end", TokenType.END);
-        reservedWords.put("repeat", TokenType.REPEAT);
-        reservedWords.put("until", TokenType.UNTIL);
-        reservedWords.put("read", TokenType.READ);
-        reservedWords.put("write", TokenType.WRITE);
+        reservedWords.put("if", Token.IF);
+        reservedWords.put("then", Token.THEN);
+        reservedWords.put("else", Token.ELSE);
+        reservedWords.put("end", Token.END);
+        reservedWords.put("repeat", Token.REPEAT);
+        reservedWords.put("until", Token.UNTIL);
+        reservedWords.put("read", Token.READ);
+        reservedWords.put("write", Token.WRITE);
     }
 
     /* getNextChar fetches the next non-blank character from
@@ -65,7 +66,7 @@ public class Scanner {
                     /* FileReader.read() returns the number of characters read,
                     or -1 if the end of the stream has been reached */
                 if (fileReader.read(lineBuf, 0, BUFLEN - 1) > 0) {
-                    if (Globals.EchoSource) {
+                    if (Globals.echoSource) {
                         /* TODO: print the output to a file "listing". */
                         System.out.println(Globals.lineno + ": " + new String(lineBuf));
                     }
@@ -90,6 +91,138 @@ public class Scanner {
     }
 
 
+    /* The primary function of the scanner: returns the
+     * next token in source file
+     */
+    Token getToken(FileReader fileReader) throws FileNotFoundException {
+
+        /* index for storing into tokenString */
+        int tokenStringIndex = 0;
+
+        /* holds current token to be returned */
+        Token currentToken = null;
+
+        /* current state - always begins at START */
+        State state = State.START;
+
+        /* flag to indicate save to tokenString */
+        boolean save;
+
+        while (state != State.DONE) {
+            char c = (char) getNextChar(fileReader);
+            save = true;
+
+            switch (state) {
+                case START:
+                    if (Character.isDigit(c)) {
+                        state = State.INNUM;
+                    } else if (Character.isLetter(c)) {
+                        state = State.INID;
+                    } else if (c == ':') {
+                        state = State.INASSIGN;
+                    } else if ((c == ' ') || (c == '\t') || (c == '\n')) {
+                        save = false;
+                    } else if (c == '{') {
+                        save = false;
+                        state = State.INCOMMENT;
+                    } else {
+                        state = State.DONE;
+                        switch (c) {
+                            case (char) Globals.EOF:
+                                save = false;
+                                currentToken = Token.ENDFILE;
+                                break;
+                            case '=':
+                                currentToken = Token.EQ;
+                                break;
+                            case '<':
+                                currentToken = Token.LT;
+                                break;
+                            case '+':
+                                currentToken = Token.PLUS;
+                                break;
+                            case '-':
+                                currentToken = Token.MINUS;
+                                break;
+                            case '*':
+                                currentToken = Token.TIMES;
+                                break;
+                            case '/':
+                                currentToken = Token.OVER;
+                                break;
+                            case '(':
+                                currentToken = Token.LPAREN;
+                                break;
+                            case ')':
+                                currentToken = Token.RPAREN;
+                                break;
+                            case ';':
+                                currentToken = Token.SEMI;
+                                break;
+                            default:
+                                currentToken = Token.ERROR;
+                                break;
+                        }
+                    }
+                    break;
+                case INCOMMENT:
+                    save = false;
+                    if (c == (char) Globals.EOF) {
+                        state = State.DONE;
+                        currentToken = Token.ENDFILE;
+                    } else if (c == '}') state = State.START;
+                    break;
+                case INASSIGN:
+                    state = State.DONE;
+                    if (c == '=') {
+                        currentToken = Token.ASSIGN;
+                    } else { /* backup in the input */
+                        ungetNextChar();
+                        save = false;
+                        currentToken = Token.ERROR;
+                    }
+                    break;
+                case INNUM:
+                    if (!Character.isDigit(c)) { /* backup in the input */
+                        ungetNextChar();
+                        save = false;
+                        state = State.DONE;
+                        currentToken = Token.NUM;
+                    }
+                    break;
+                case INID:
+                    if (!Character.isLetter(c)) { /* backup in the input */
+                        ungetNextChar();
+                        save = false;
+                        state = State.DONE;
+                        currentToken = Token.ID;
+                    }
+                    break;
+                case DONE:
+                default: /* should never happen */
+                    /* TODO: also pring out listing in error message. */
+                    System.out.println("Scanner Bug: state=" + state.name());
+                    state = State.DONE;
+                    currentToken = Token.ERROR;
+                    break;
+            }
+            if ((save) && (tokenStringIndex <= MAXTOKENLEN)) {
+                tokenString[tokenStringIndex++] = c;
+            }
+            if (state == State.DONE) {
+                tokenString[tokenStringIndex] = '\0';
+                if (currentToken == Token.ID) {
+                    currentToken = reservedWords.get(new String(tokenString));
+                }
+            }
+        }
+        if (Globals.traceScan) {
+            /* TODO: also pring out listing in error message. */
+            System.out.println("\t: " + Globals.lineno);
+            Utils.printToken(currentToken, new String(tokenString));
+        }
+        return currentToken;
+    }
 
 
 }
