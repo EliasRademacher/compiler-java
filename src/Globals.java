71f7ee50d0c8bd4d5321beb5a57f8d7b8a487073
EliasRademacher <rademace@onid.oregonstate.edu>
2017-03-08 15:55:45 -0800

    Made Globals into a singleton.

--- src/Globals.java
+++ src/Globals.java
@@ -1,22 +1,68 @@
-/**
- * Created by Elias on 3/4/2017.
- */
-public class Globals {
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public final class Globals {
+
     /* source line number for listing */
     public static int lineno;
     public static final int EOF = -1;
 
+    /* source code text file */
+    private FileWriter source;
+
     /* listing output text file */
-//    public FILE listing;
+    private FileWriter listing;
+
+
+    private static Globals instance = null;
+
+    private Globals() {
+        try {
+            this.source = new FileWriter("./source.txt");
+            this.listing = new FileWriter("./listing.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Globals getInstance() {
+        if (instance == null) {
+            instance = new Globals();
+        }
+        return instance;
+    }
+
+    public void writeListing(char[] cbuf) {
+        try {
+            this.listing.write(cbuf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
 
     /**************************************************/
     /***********   Flags for tracing       ************/
     /**************************************************/
 
-    /* EchoSource = TRUE causes the source program to
+    /* echoSource = TRUE causes the source program to
      * be echoed to the listing file with line numbers
      * during parsing
      */
-    public static boolean EchoSource;
+    public static boolean echoSource;
+
+
+    /* traceScan = TRUE causes token information to be
+     * printed to the listing file as each token is
+     * recognized by the scanner
+     */
+    public static boolean traceScan;
+
+
 
 }
--- src/Utils.java
+++ src/Utils.java
@@ -2,4 +2,75 @@
  * Created by Elias on 3/4/2017.
  */
 public class Utils {
+
+
+    /* Procedure printToken prints a token
+         * and its lexeme to the listing file
+         */
+    static void printToken(Token token, final String tokenString) {
+        switch (token) {
+            case IF:
+            case THEN:
+            case ELSE:
+            case END:
+            case REPEAT:
+            case UNTIL:
+            case READ:
+            case WRITE:
+//                //Globals.writeListing(tokenString);
+//
+//                fprintf(listing,
+//                        "reserved word: %s\n", tokenString);
+//                break;
+//            case ASSIGN:
+//                fprintf(listing, ":=\n");
+//                break;
+//            case LT:
+//                fprintf(listing, "<\n");
+//                break;
+//            case EQ:
+//                fprintf(listing, "=\n");
+//                break;
+//            case LPAREN:
+//                fprintf(listing, "(\n");
+//                break;
+//            case RPAREN:
+//                fprintf(listing, ")\n");
+//                break;
+//            case SEMI:
+//                fprintf(listing, ";\n");
+//                break;
+//            case PLUS:
+//                fprintf(listing, "+\n");
+//                break;
+//            case MINUS:
+//                fprintf(listing, "-\n");
+//                break;
+//            case TIMES:
+//                fprintf(listing, "*\n");
+//                break;
+//            case OVER:
+//                fprintf(listing, "/\n");
+//                break;
+//            case ENDFILE:
+//                fprintf(listing, "EOF\n");
+//                break;
+//            case NUM:
+//                fprintf(listing,
+//                        "NUM, val= %s\n", tokenString);
+//                break;
+//            case ID:
+//                fprintf(listing,
+//                        "ID, name= %s\n", tokenString);
+//                break;
+//            case ERROR:
+//                fprintf(listing,
+//                        "ERROR: %s\n", tokenString);
+//                break;
+//            default: /* should never happen */
+//                fprintf(listing, "Unknown token: %d\n", token);
+        }
+    }
+
+
 }
