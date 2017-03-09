852b948371eeef2579134e90f412c985dea76e1a
EliasRademacher <rademace@onid.oregonstate.edu>
2017-03-08 16:16:56 -0800

    Fixed Utils.printToken()

--- src/Utils.java
+++ src/Utils.java
@@ -5,8 +5,8 @@ public class Utils {
 
 
     /* Procedure printToken prints a token
-         * and its lexeme to the listing file
-         */
+     * and its lexeme to the listing file.
+     */
     static void printToken(Token token, final String tokenString) {
         switch (token) {
             case IF:
@@ -18,55 +18,51 @@ public class Utils {
             case READ:
             case WRITE:
                 Listing.getInstance().write(tokenString);
-
-//                break;
-//            case ASSIGN:
-//                fprintf(listing, ":=\n");
-//                break;
-//            case LT:
-//                fprintf(listing, "<\n");
-//                break;
-//            case EQ:
-//                fprintf(listing, "=\n");
-//                break;
-//            case LPAREN:
-//                fprintf(listing, "(\n");
-//                break;
-//            case RPAREN:
-//                fprintf(listing, ")\n");
-//                break;
-//            case SEMI:
-//                fprintf(listing, ";\n");
-//                break;
-//            case PLUS:
-//                fprintf(listing, "+\n");
-//                break;
-//            case MINUS:
-//                fprintf(listing, "-\n");
-//                break;
-//            case TIMES:
-//                fprintf(listing, "*\n");
-//                break;
-//            case OVER:
-//                fprintf(listing, "/\n");
-//                break;
-//            case ENDFILE:
-//                fprintf(listing, "EOF\n");
-//                break;
-//            case NUM:
-//                fprintf(listing,
-//                        "NUM, val= %s\n", tokenString);
-//                break;
-//            case ID:
-//                fprintf(listing,
-//                        "ID, name= %s\n", tokenString);
-//                break;
-//            case ERROR:
-//                fprintf(listing,
-//                        "ERROR: %s\n", tokenString);
-//                break;
-//            default: /* should never happen */
-//                fprintf(listing, "Unknown token: %d\n", token);
+                break;
+            case ASSIGN:
+                Listing.getInstance().write(":=\n");
+                break;
+            case LT:
+                Listing.getInstance().write("<\n");
+                break;
+            case EQ:
+                Listing.getInstance().write("=\n");
+                break;
+            case LPAREN:
+                Listing.getInstance().write("(\n");
+                break;
+            case RPAREN:
+                Listing.getInstance().write(")\n");
+                break;
+            case SEMI:
+                Listing.getInstance().write(";\n");
+                break;
+            case PLUS:
+                Listing.getInstance().write("+\n");
+                break;
+            case MINUS:
+                Listing.getInstance().write("-\n");
+                break;
+            case TIMES:
+                Listing.getInstance().write("*\n");
+                break;
+            case OVER:
+                Listing.getInstance().write("/\n");
+                break;
+            case ENDFILE:
+                Listing.getInstance().write("EOF\n");
+                break;
+            case NUM:
+                Listing.getInstance().write("NUM, val= %s\n" + tokenString);
+                break;
+            case ID:
+                Listing.getInstance().write("ID, name= %s\n" + tokenString);
+                break;
+            case ERROR:
+                Listing.getInstance().write("ERROR: %s\n" + tokenString);
+                break;
+            default: /* should never happen */
+                Listing.getInstance().write("Unknown token: \n" + token);
         }
     }
 
