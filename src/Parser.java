bb27831ad1130c9b1d97ebb088ce06c63de28da9
EliasRademacher <rademace@onid.oregonstate.edu>
2017-03-11 20:48:00 -0800

    Added method match() to Parser.

--- src/Parser.java
+++ src/Parser.java
@@ -1,12 +1,34 @@
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Parser {
+
+    /* holds current token */
+    private Token token;
+
+    private Scanner scanner = new Scanner();
 
     private void syntaxError(String message) {
         Listing.getInstance().write(
                 "\n>>> Syntax error at line" + Globals.lineno + ": " + message
         );
         Globals.Error = true;
+    }
+
+    /* TODO: pass in token as parameter to match()? */
+    private void match(Token expectedToken, FileReader fileReader) throws FileNotFoundException {
+        if (token == expectedToken) {
+            token = scanner.getToken(fileReader);
+        }
+
+        else {
+            syntaxError("unexpected token -> ");
+
+        /* TODO: how can I associate a tokenString with a Token?
+         * Should I create a Token class? */
+            Utils.printToken(token, "replace with tokenString");
+            Listing.getInstance().write("      ");
+        }
     }
 
 }
