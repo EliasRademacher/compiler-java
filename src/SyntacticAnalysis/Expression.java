a616b62b0d97188dd892aafd38dbdae4fdec0941
EliasRademacher <rademace@onid.oregonstate.edu>
2017-03-14 18:14:29 -0700

    Added some comments to Statement and Expression.

--- src/SyntacticAnalysis/Expression.java
+++ src/SyntacticAnalysis/Expression.java
@@ -1,7 +1,15 @@
 package SyntacticAnalysis;
 
 /**
- * Can be of type "OpK", "ConstK", or "IdK".
+ * An expression in is a combination of one or more
+ * explicit values, constants, variables, operators, and functions.
+ * An expression is an internal component of a statement.
+ * "2+3", "y+6", and "4!=4" are examples of expressions.
+ *
+ *
+ * Can be of type "OpK": (EQ, LT, PLUS, MINUS, TIMES, OVER);
+ * "ConstK": (INT); or
+ * "IdK": (ID)
  */
 public class Expression extends ParseTreeElement {
 }
--- src/SyntacticAnalysis/Statement.java
+++ src/SyntacticAnalysis/Statement.java
@@ -1,7 +1,14 @@
 package SyntacticAnalysis;
 
 /**
- * Can be of type "IfK", "RepeatK", "AssignK", "ReadK", or "WriteK".
+ * A statement is the smallest standalone element of an
+ * imperative programming language that expresses some action to be carried out.
+ *
+ * Can be thought of as everything that can make up a line
+ * (or several lines) of code.
+ *
+ * Can be of type IF, WHILE, WRITE, or ASSIGN.
+ *
  */
 public class Statement extends ParseTreeElement {
 }
