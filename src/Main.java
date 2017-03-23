8d732044c20a4e8eada1d94dff06e8cfa88eaf84
EliasRademacher <rademace@onid.oregonstate.edu>
2017-03-23 00:38:30 -0700

    Call scanner, parser, and checker from main method.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import LexicalAnalysis.Scanner;
+import SemanticAnalysis.SemanticAnalyzer;
+import SyntacticAnalysis.Parser;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        FileReader simpleFunctionFileReader = null;
+
+        try {
+            simpleFunctionFileReader = new FileReader(args[0]);
+        } catch (FileNotFoundException e) {
+            System.out.println("could not find the file specified.");
+            return;
+        }
+
+        Scanner scanner = new Scanner(simpleFunctionFileReader);
+
+        Parser parser = new Parser(scanner);
+        DefaultMutableTreeNode AST = parser.parse();
+
+        SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer(AST);
+
+        /* Build Symbol Table Tree. */
+        semanticAnalyzer.checkIdentifiers();
+
+        /* Perform Type Checking. */
+        semanticAnalyzer.traverseTypeCheck();
+
     }
 }
